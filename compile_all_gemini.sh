19317f27c9eefaa2655a23457210440cfd951839
TijnBerends <60062177+TijnBerends@users.noreply.github.com>
2021-03-12 09:13:58 +0100

    Update compile_all_gemini.sh
    Updated to v1.1.1

--- compile_all_gemini.sh
+++ compile_all_gemini.sh
@@ -1,13 +1,13 @@
-rm -f src_v1.1.0/liblapack.so
-ln -s /usr/lib64/liblapack.so.3.4.2 src_v1.1.0/liblapack.so
+rm -f src_v1.1.1/liblapack.so
+ln -s /usr/lib64/liblapack.so.3.4.2 src_v1.1.1/liblapack.so
 
 module load mpi/openmpi-x86_64
 
-cd src_v1.1.0
+cd src_v1.1.1
 #make clean
 make all
 cd ..
 
 rm -f IMAU_ICE_program
 
-mv src_v1.1.0/IMAU_ICE_program .
+mv src_v1.1.1/IMAU_ICE_program .
